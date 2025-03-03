88a829079dd34c1d6582dee2431c0e9cf9764558
Quanisme122 <quanbui9925@gmail.com>
2025-03-03 16:43:34 +0700

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,17 +1,13 @@
-
-type Params = { slug: string };
-
-export async function generateMetadata({ params }: { params: Params }) {
-  return { title: `Post: ${params.slug}` };
-}
+type Params = { slug: string[] };
 
 export default function Page({ params }: { params: Params }) {
+  const { slug } = params;
+
   return (
     <>
-      <h1>Slug: {params.slug}</h1>
+      <h1>Slug: {slug.join(" / ")}</h1>
       <p>HoleTex</p>
       <p>Welcome to HCM</p>
     </>
   );
 }
-
