181a36b429d18ff4b3da7bdd247d8757606dbbc4
Quanisme122 <Quanbui9925@gmail.com>
2025-03-03 16:02:27 +0700

    init project

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,15 @@
+type Params = { slug: string };
+
+export async function generateMetadata({ params }: { params: Params }) {
+  return { title: `Post: ${params.slug}` };
+}
+
+export default function Page({ params }: { params: Params }) {
+  return (
+    <>
+      <h1>Slug: {params.slug}</h1>
+      <p>HoleTex</p>
+      <p>Welcome to HCM</p>
+    </>
+  );
+}
